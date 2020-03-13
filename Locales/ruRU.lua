eab28be2ba7aae8051aec6761236800670a0c152
Zidras <10605951+Zidras@users.noreply.github.com>
2020-03-13 18:49:03 +0000

    Add files via upload

---
+++ Locales/ruRU.lua
@@ -0,0 +1,22 @@
+-- Russian localization file for ruRU.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0-ElvUI")
+local L = AceLocale:NewLocale("ElvUI", "ruRU")
+if not L then return end
+
+L["Change equipment sets when you change your spec."] = "Change equipment sets when you change your spec." -- Requires localization
+L["Change Specialization"] = "Change Specialization" -- Requires localization
+L["Choose Loot Specialization"] = "Choose Loot Specialization" -- Requires localization
+L["Choose the equipment set to use for your primary spec."] = "Choose the equipment set to use for your primary spec." -- Requires localization
+L["Choose the equipment set to use for your secondary spec."] = "Choose the equipment set to use for your secondary spec." -- Requires localization
+L["Improved Spec Switch"] = "Improved Spec Switch" -- Requires localization
+L["Left Click"] = "Left Click" -- Requires localization
+L["No Change"] = "No Change" -- Requires localization
+L["No Specialization"] = "No Specialization" -- Requires localization
+L["Primary Talents"] = "Primary Talents" -- Requires localization
+L["Right Click"] = "Right Click" -- Requires localization
+L["Secondary Talents"] = "Secondary Talents" -- Requires localization
+L["Shift + Left Click"] = "Shift + Left Click" -- Requires localization
+L["Show Hint"] = "Show Hint" -- Requires localization
+L["Show the hint in the tooltip."] = "Show the hint in the tooltip." -- Requires localization
+L["Swap Equipment Sets"] = "Swap Equipment Sets" -- Requires localization
+L["Toggle Talents Frame"] = "Toggle Talents Frame" -- Requires localization
