d37fc3ac17649cbe5aeb0fc8e82657915333c2c6
Zidras <10605951+Zidras@users.noreply.github.com>
2020-03-14 16:11:38 +0000

    Update ElvUI_ImprovedSpecSwitch.lua
    Cleanup and Spec correctly displayed on login and new world

--- ElvUI_ImprovedSpecSwitch/ElvUI_ImprovedSpecSwitch.lua
+++ ElvUI_ImprovedSpecSwitch/ElvUI_ImprovedSpecSwitch.lua
@@ -4,7 +4,6 @@
 -------------------------------------------------------------------------------
 local E, L, V, P, G = unpack(ElvUI) --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local DT = E:GetModule("DataTexts")
-local L = LibStub("AceLocale-3.0-ElvUI"):GetLocale("ElvUI", false)
 local EP = LibStub("LibElvUIPlugin-1.0")
 
 --Lua functions
@@ -17,48 +16,45 @@ local GetEquipmentSetInfo = GetEquipmentSetInfo
 local GetActiveTalentGroup = GetActiveTalentGroup
 local GetTalentTabInfo = GetTalentTabInfo
 local UseEquipmentSet = UseEquipmentSet
-local GetNumTalentGroups = GetNumTalentGroups
 local SetActiveTalentGroup = SetActiveTalentGroup
 local ToggleTalentFrame = ToggleTalentFrame
+
+local MAX_TALENT_TABS = MAX_TALENT_TABS
+
 local lastPanel, active, activeSet
-local displayString, displayString2 noSpecString = ""
-local activeString = ("|cff00ff00%s|r"):format("Active") -- ACTIVE_PETS doesn't exist and didn't find an "Active" global
-local inactiveString = ("|cffff0000%s|r"):format(_G.FACTION_INACTIVE)
+local displayString, displayString2, noSpecString = ""
+local activeString = format("|cff00ff00%s|r", "Active") -- ACTIVE_PETS doesn't exist and didn't find an "Active" global
+local inactiveString = format("|cffff0000%s|r", FACTION_INACTIVE)
 
 local function ColorizeSettingName(name)
 	return format("|cff1784d1%s|r", name)
 end
 
 local function AddTexture(texture)
-	texture = texture and "|T"..texture..":20:20:0:0:64:64:4:55:4:55|t" or ""
-	return texture
+	return texture and "|T"..texture..":20:20:0:0:64:64:4:55:4:55|t" or ""
 end
 
 local function GetCurrentEquipmentSet()
-	if GetNumEquipmentSets() == 0 then return false end
 	for i = 1, GetNumEquipmentSets() do
-		local name, _, _, isEquipped, _, _, _, _, _ = GetEquipmentSetInfo(i)
-		if isEquipped then
-			return name
-		end
+		local name = GetEquipmentSetInfo(i)
+		return name
 	end
-	return false
 end
 
 local function OnEvent(self, event)
 	lastPanel = self
 
 	local specIndex = GetActiveTalentGroup()
-	if not specIndex then
-		self.text:SetFormattedText(noSpecString, L["No Specialization"])
-		return
-	end	
 	
 	active = specIndex
 	activeSet = GetCurrentEquipmentSet()
 
 	local _, specName, talent = E:GetTalentSpecInfo()
-	self.text:SetFormattedText(displayString2, AddTexture(talent), specName)
+	if specName == "None" then
+		self.text:SetFormattedText("%s", L["No Specialization"])
+	else
+		self.text:SetFormattedText("%s %s", AddTexture(talent), specName)
+	end
 
 	-- determine if we need to change the equipment set
 	if self.clicked and E.db.impss.switch and GetNumEquipmentSets() > 0 then
@@ -73,24 +69,25 @@ end
 local function OnEnter(self)
 	DT:SetupTooltip(self)
 
-	for i = 1, _G.MAX_TALENT_TABS do
-		local _, specName, talent = E:GetTalentSpecInfo()
+	for i = 1, MAX_TALENT_TABS do
+		local _, specName = E:GetTalentSpecInfo()
 		local name, icon = GetTalentTabInfo(i)
 			DT.tooltip:AddLine(join(" ", AddTexture(icon), format(displayString, name), (specName == name and activeString or inactiveString)), 1, 1, 1)
 	end
-	
-	if E.db.impss.hint == true then
+
+	if E.db.impss.hint then
 		DT.tooltip:AddLine(" ")
 		DT.tooltip:AddDoubleLine(L["Left Click"], L["Change Specialization"], 1, 1, 1, 1, 1, 0)
 		DT.tooltip:AddDoubleLine(L["Right Click"], L["Toggle Talents Frame"], 1, 1, 1, 1, 1, 0)
 	end
-	DT.tooltip:Show()	
+
+	DT.tooltip:Show()
 end
 
 local function OnClick(self, button)
 	local specIndex = GetActiveTalentGroup()
-	if not specIndex then return end
-	
+	if not specIndex then return end -- always have value?
+
 	if button == "LeftButton" then
 		SetActiveTalentGroup(active == 1 and 2 or 1)
 		self.clicked = true
@@ -103,6 +100,7 @@ local function ValueColorUpdate(hex)
 	displayString = join("", "|cffFFFFFF%s:|r ")
 	displayString2 = join("", "%s ", hex, "%s|r")
 	noSpecString = join("", hex, "%s|r")
+
 	if lastPanel ~= nil then
 		OnEvent(lastPanel)
 	end
@@ -130,7 +128,7 @@ local function InsertOptions()
 				name = L["Swap Equipment Sets"],
 				desc = L["Change equipment sets when you change your spec."],
 				disabled = function()
-					if not GetActiveTalentGroup(false, false) then 
+					if not GetActiveTalentGroup(false, false) then
 						return true
 					else
 						return false
@@ -140,7 +138,7 @@ local function InsertOptions()
 			primary = {
 				type = "select",
 				order = 2,
-				name = L["Primary Talents"], --For Primary Spec Name add: select(2, E:GetTalentSpecInfo()) or 
+				name = L["Primary Talents"], --For Primary Spec Name add: select(2, E:GetTalentSpecInfo()) or
 				desc = L["Choose the equipment set to use for your primary spec."],
 				disabled = function() return not E.db.impss.switch end,
 				values = function()
@@ -148,7 +146,7 @@ local function InsertOptions()
 						["none"] = L["No Change"],
 					}
 					for i = 1, GetNumEquipmentSets() do
-						local name, _, _, _, _, _, _, _, _ = GetEquipmentSetInfo(i)
+						local name = GetEquipmentSetInfo(i)
 						if name then
 							sets[name] = name
 						end
@@ -168,7 +166,7 @@ local function InsertOptions()
 						["none"] = L["No Change"],
 					}
 					for i = 1, GetNumEquipmentSets() do
-						local name, _, _, _, _, _, _, _, _ = GetEquipmentSetInfo(i)
+						local name = GetEquipmentSetInfo(i)
 						if name then
 							sets[name] = name
 						end
@@ -188,4 +186,4 @@ local function InsertOptions()
 end
 
 EP:RegisterPlugin(..., InsertOptions)
-DT:RegisterDatatext(L["Improved Spec Switch"], {"PLAYER_ENTERING_WORLD", "CHARACTER_POINTS_CHANGED", "PLAYER_TALENT_UPDATE", "ACTIVE_TALENT_GROUP_CHANGED"}, OnEvent, nil, OnClick, OnEnter, nil, ColorizeSettingName(L["Improved Spec Switch"]))
+DT:RegisterDatatext(L["Improved Spec Switch"], {"PLAYER_ENTERING_WORLD", "PLAYER_ALIVE", "CHARACTER_POINTS_CHANGED", "PLAYER_TALENT_UPDATE", "ACTIVE_TALENT_GROUP_CHANGED"}, OnEvent, nil, OnClick, OnEnter, nil, ColorizeSettingName(L["Improved Spec Switch"]))
